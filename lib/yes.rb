adce9fc7de38386268815ce12de9130ad88b6cbf
7rans <transfire@gmail.com>
2011-06-27 12:13:02 -0400

    Output a generalized node representation.

--- lib/yes.rb
+++ lib/yes.rb
@@ -1,4 +1,11 @@
 # YAML Easy Schema
+#
+# Issues
+#
+# * Matching fully-quaified tags.
+# * Handling namespaces.
+# * Handling YAML version directive.
+#
 class YES
 
   #
@@ -55,7 +62,7 @@ class YES
   def validate_required(spec, nodes)
     return unless spec['required']
     if spec['required'] && nodes.empty?
-      @edit << [:required, spec]
+      @edit << ['required', spec]
     end
   end
 
@@ -65,11 +72,11 @@ class YES
     return unless spec['range']
     min, max = spec['range'].split('..')
     if nodes.size < min.to_i
-      @edit << [:range, spec]
+      @edit << ['range', spec, nodes.size]
     end
     next if max == 'n'
     if nodes.size > max.to_i
-      @edit << [:range, spec]
+      @edit << ['range', spec, nodes.size]
     end
   end
 
@@ -80,7 +87,7 @@ class YES
     nodes.each do |node|
       next if type_match(node, type)
       # TODO: more types
-      @edit << [:type, spec, node]
+      @edit << ['type', spec, catholic_node(node)]
     end
   end  
 
@@ -90,7 +97,7 @@ class YES
     pattern = Regexp.new(spec['pattern'])
     nodes.each do |node|
       next if pattern =~ node.value
-      @edit << [:pattern, spec, node]
+      @edit << ['pattern', spec, catholic_node(node)]
     end
   end
 
@@ -100,15 +107,13 @@ class YES
     fnmatch = spec['fnmatch']
     nodes.each do |node|
       next if File.fnmatch(fnmatch, node.value)
-      @edit << [:fnmatch, spec, node]
+      @edit << ['fnmatch', spec, catholic_node(node)]
     end
   end
 
   #
   def type_match(node, type)
-    type_id = fixed_type_id(node)
-   
-    node_type = type_id.split(':').last
+    node_type = base_type_id(node)
     pick_type = TYPE_MATCH[type] || [type]
     pick_type.any?{ |t| t == node_type }
   end
@@ -121,6 +126,23 @@ class YES
     else
       node.kind.to_s
     end
+  end
+
+  #
+  def base_type_id(node)
+    fixed_type_id(node).split(':').last
+  end
+
+  # Covert a YAML node (Syck) node into a generic representation.
+  #
+  # NOTE: Should `style` be part of this? Also, is `kind` the proper term?
+  def catholic_node(node)
+    n = {}
+    n['tag']   = node.type_id #base_type_id(node)
+    n['kind']  = node.kind.to_s
+    n['value'] = node.value
+    n['style'] = node.instance_variable_get('@style').to_s
+    n
   end
 
   #
