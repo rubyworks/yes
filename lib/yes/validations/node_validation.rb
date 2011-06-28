bae575c6bd1995b4e41edf674531051a14c05cd0
7rans <transfire@gmail.com>
2011-06-28 18:54:27 -0400

    Add node shortcuts to NodeValidation class.

--- lib/yes/validations/node_validation.rb
+++ lib/yes/validations/node_validation.rb
@@ -4,15 +4,25 @@ module YES
   class NodeValidation < AbstractValidation
 
     #
+    def initialize(spec, node)
+      @spec = spec
+      @node = node
+    end
+
+    #
     attr :spec
 
     #
     attr :node
 
-    #
-    def initialize(spec, node)
-      @spec = spec
-      @node = node
+    # Get the applicable node's tag.
+    def tag
+      node.type_id
+    end
+
+    # Get the applicable node's value.
+    def value
+      node.value
     end
 
   end
