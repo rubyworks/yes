e7c3b6da84c66195cc93900f6e1e1dbb64596c42
7rans <transfire@gmail.com>
2011-07-13 13:07:22 -0400

    Add simplecov configuration for QED. [admin]

---
+++ .qed/cov.rb
@@ -0,0 +1,8 @@
+require 'simplecov'
+SimpleCov.start do
+  coverage_dir 'log/coverage'
+  #add_group "Shared" do |src_file|
+  #  /lib\/dotruby\/v(\d+)(.*?)$/ !~ src_file.filename
+  #end
+  #add_group "Revision 0", "lib/dotruby/v0"
+end
